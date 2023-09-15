a36452e627d5d30df92aaed76551140b51e47ef7
McHaillet <martenchaillet@gmail.com>
2023-09-15 11:46:05 +0200

    score map and label dataset

--- extractor/data/datasets.py
+++ extractor/data/datasets.py
@@ -1,11 +1,13 @@
 import torch.utils.data as data
 import pathlib
 import mrcfile
-import tiler
+import numpy as np
+import numpy.typing as npt
+from tiler import Tiler
 
 
 class ScoreData(data.Dataset):
-    def __init__(self, path: pathlib.Path, patch_size: int):
+    def __init__(self, path: pathlib.Path, patch_size: int = 64, patch_overlap: int = 32):
         """
         Dataset structure for extractor!
 
@@ -21,23 +23,71 @@ class ScoreData(data.Dataset):
         Parameters
         ----------
         path: pathlib.Path
-            input directory with score maps (MRC) and ground truths (MRC) with matching names
+            Input directory with score maps (MRC) and ground truths (MRC) with matching names.
         patch_size: int
-            size of cubic training patches
+            Size of cubic training patches.
+        patch_overlap: int
+            To what extend the patches should overlap.
         """
         self.path = path
-        tomo_ids = [p.name.strip('_scores.mrc') for p in self.path.iterdir() if p.name.endswith('_scores.mrc')]
         # viable tomo_ids that have both a _scores and _gt MRC are stored in self.tomo_ids
+        tomo_ids = [p.name.strip('_scores.mrc') for p in self.path.iterdir() if p.name.endswith('_scores.mrc')]
         self.tomo_ids = [tid for tid in tomo_ids if self.path.joinpath(tid + '_gt.mrc').exists()]
         self.tomo_count = len(self.tomo_ids)
 
-        # load all data
+        # load score volumes and label volumes; assume the user provided valid data
+        self.score_volumes, self.label_volumes = [], []
+        for tid in tomo_ids:
+            with mrcfile.open(self.path.joinpath(tid + '_scores.mrc'), mode='r', permissive=True) as mrc:
+                self.score_volumes.append(mrc.data.astype(np.float32).copy())
+            with mrcfile.open(self.path.joinpath(tid + '_gt.mrc'), mode='r', permissive=True) as mrc:
+                self.label_volumes.append(mrc.data.astype(np.float32).copy())
 
         # split into tiles using patch_size and make a large index of all tiles
+        self.tilers = []
+        for i in range(self.tomo_count):
 
-    def __len__(self):
-        return self.tile_count
+            self.tilers.append(Tiler(data_shape=self.score_volumes[i].shape,
+                                     tile_shape=(patch_size, patch_size, patch_size),
+                                     overlap=(patch_overlap, patch_overlap, patch_overlap),
+                                     mode='reflect'))
 
-    def __getitem__(self, idx):
-        pass
+        self.split = [len(t) for t in self.tilers]
 
+    def __len__(self) -> int:
+        """
+        Returns
+        -------
+        int
+            The total number of patches in this score dataset.
+        """
+        return sum(self.split)
+
+    def __getitem__(self, idx: int) -> tuple[npt.NDArray[float], npt.NDArray[float]]:
+        """
+        Parameters
+        ----------
+        idx: int
+            Index of patch that should be returned.
+
+        Returns
+        -------
+        patch_score, patch_label: tuple[npt.NDArray[float], npt.NDArray[float]]
+            Return a patch with corresponding label from the dataset.
+        """
+
+        tomo = 0
+        for s in self.split:
+            idx -= s
+            if idx >= 0:
+                tomo += 1
+                continue
+            else:
+                idx += s
+                break
+
+        # indexing numpy array with [None] adds extra channel dimension
+        patch_score = self.tilers[tomo].get_tile(self.score_volumes[tomo], idx)[None]
+        patch_label = self.tilers[tomo].get_tile(self.label_volumes[tomo], idx)[None]
+
+        return patch_score, patch_label
