70ff34a95cf787a037dd965c7ade781bcf69c56f
Marten <58044494+McHaillet@users.noreply.github.com>
2023-11-15 15:29:30 +0100

    reduce model further

--- extractor/models/peakfinder.py
+++ extractor/models/peakfinder.py
@@ -2,20 +2,17 @@ import torch.nn as nn
 
 
 class PeakFinder(nn.Module):
-    def __init__(self, channels=8, kernel_size=3):
+    def __init__(self, channels_input=2, channels_first=8, channels_second=4, channels_output=2, kernel_size=3):
         super(PeakFinder, self).__init__()
 
         layers = [
-            nn.Conv3d(1, channels, kernel_size=(kernel_size, ) * 3, padding=1),
-            nn.BatchNorm3d(channels),
+            nn.Conv3d(channels_input, channels_first, kernel_size=(kernel_size, ) * 3, padding=1),
+            nn.BatchNorm3d(channels_first),
             nn.ReLU(inplace=True),
-            nn.Conv3d(channels, channels, kernel_size=(kernel_size, ) * 3, padding=1),
-            nn.BatchNorm3d(channels),
+            nn.Conv3d(channels_first, channels_second, kernel_size=(kernel_size, ) * 3, padding=1),
+            nn.BatchNorm3d(channels_second),
             nn.ReLU(inplace=True),
-            nn.Conv3d(channels, channels, kernel_size=(kernel_size,) * 3, padding=1),
-            nn.BatchNorm3d(channels),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(channels, 2, kernel_size=(1, ) * 3)
+            nn.Conv3d(channels_second, channels_output, kernel_size=(1, ) * 3)
         ]
 
         self.network = nn.Sequential(*layers)
